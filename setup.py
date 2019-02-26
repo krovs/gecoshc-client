fe43809a27fb9e463641810797c20524291c7f48
Alfonso de Cala <alfonso@el-magnifico.org>
2019-02-26 11:19:39 +0100

    New 1.1.0 version

--- setup.py
+++ setup.py
@@ -134,7 +134,7 @@ class Clean(Command):
 
 DistUtilsExtra.auto.setup(
     name='helpchannel',
-    version='1.0.1',
+    version='1.1.0',
     license='GPL-2',
     author='GECOS Team',
     author_email='gecos@guadalinex.org',
